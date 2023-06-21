35e4b6519b357f3ca2b6f63670c4f5b10e12d416
LAPTOP-M8V9U3AJ\davic <davicho.DL1@gmail.com>
2023-06-21 18:46:43 -0500

    Lascano Aldahir..

--- src/gato.java
+++ src/gato.java
@@ -6,7 +6,7 @@ public class gato extends felinos{
         super();}
 
     public gato(String nombre_felino, String tipo, float estatura, String color, String alimentacion) {
-        super(nombre_felino, tiempo_de_vida, tipo, estatura);
+        super(nombre_felino, tipo, estatura);
         this.color = color;
         this.alimentacion = alimentacion;}
 
@@ -26,6 +26,19 @@ public class gato extends felinos{
         this.alimentacion = alimentacion;
     }
 
+    public void imprimir_gato(){
+        System.out.println("El nombre del felino es: ");
+        System.out.println(nombre_felino);
+        System.out.println("El tipo de felino es: ");
+        System.out.println(tipo);
+        System.out.println("La estatura del felino es: ");
+        System.out.println(estatura);
+        System.out.println("El color del gato es: ");
+        System.out.println(color);
+        System.out.println("Su alimentación es : ");
+        System.out.println(alimentacion);
 
 
-}
+
+
+    }
