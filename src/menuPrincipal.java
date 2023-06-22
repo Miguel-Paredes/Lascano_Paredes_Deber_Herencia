b61176cbc0654d41174a9cf99ad1d07b8e7d5825
AzureAD\10800 <os.miguelparedes@gmail.com>
2023-06-21 20:32:49 -0500

    fin

--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class menuPrincipal extends felinos{
+public class menuPrincipal{
     public static void main(String[] args) {
  // creando objetos
         Scanner entrada = new Scanner(System.in);
