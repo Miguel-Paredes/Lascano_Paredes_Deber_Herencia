91a2fe7778194248088f21498067b1ef103beb1a
AzureAD\10800 <os.miguelparedes@gmail.com>
2023-06-21 19:04:34 -0500

    bucle

--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -18,34 +18,41 @@ public class menuPrincipal extends felinos{
         System.out.println("5. Tigrillo.");
         System.out.print("Seleccione una de las opciones del menu: ");
         int opt = entrada.nextInt();
-
-        switch (opt){
-            case 1 ->{
+        do{
+        switch (opt) {
+            case 1 -> {
                 System.out.println("\nCaracal");
                 System.out.println(" ");
-                celeste.imprimir_caracal();}
+                celeste.imprimir_caracal();
+            }
 
 
-            case 2 ->{
+            case 2 -> {
                 System.out.println("\nGato");
                 System.out.println(" ");
                 wilson.imprimir_gato();
 
             }
-            case 3 ->{
+            case 3 -> {
                 System.out.println("\nLince");
                 System.out.println(" ");
-            garfield.imprimir_linces();}
-            case 4 ->{
+                garfield.imprimir_linces();
+            }
+            case 4 -> {
                 System.out.println("\n Puma");
                 System.out.println(" ");
-                kira.imprimir_pumas();}
-            case 5 ->{
+                kira.imprimir_pumas();
+            }
+            case 5 -> {
                 System.out.println(("\n Tigrillo"));
                 System.out.println("");
                 jupiter.imprimir_tigrillos();
             }
-
+            case 0 -> {
+                System.out.println("Muchas Gracias");
+            }
         }
+        }
+        while (opt<0 || opt>5);
     }
 }
